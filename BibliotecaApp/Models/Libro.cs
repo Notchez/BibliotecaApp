93b1955ae228efdae075edde8ff34de0e65114d6
Tito Nochez <nochez_@hotmail.com>
2026-03-09 12:02:15 -0600

    Starting point Creacion de clases

---
+++ BibliotecaApp/Models/Libro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaApp.Models
+{
+    public class Libro
+    {
+        public int Id { get; set; }
+
+        public string Titulo { get; set; }
+
+        public string Autor { get; set; }
+
+        public string Anio { get; set; }
+
+        public bool Disponible { get; set; }
+    }
+}
