3efb3b00087f5379232fe385416c3710571cbef9
Tito Nochez <nochez_@hotmail.com>
2026-03-09 13:16:00 -0600

    Diseño de formulario

---
+++ BibliotecaApp/MainForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace BibliotecaApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnLibros = new System.Windows.Forms.Button();
+            this.btnUsuarios = new System.Windows.Forms.Button();
+            this.tnPrestamos = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(280, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(257, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sistema de Biblioteca";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnLibros
+            // 
+            this.btnLibros.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLibros.Location = new System.Drawing.Point(304, 159);
+            this.btnLibros.Name = "btnLibros";
+            this.btnLibros.Size = new System.Drawing.Size(200, 40);
+            this.btnLibros.TabIndex = 1;
+            this.btnLibros.Text = "Gestión de Libros";
+            this.btnLibros.UseVisualStyleBackColor = true;
+            // 
+            // btnUsuarios
+            // 
+            this.btnUsuarios.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUsuarios.Location = new System.Drawing.Point(304, 237);
+            this.btnUsuarios.Name = "btnUsuarios";
+            this.btnUsuarios.Size = new System.Drawing.Size(200, 40);
+            this.btnUsuarios.TabIndex = 2;
+            this.btnUsuarios.Text = "Gestión de Usuarios";
+            this.btnUsuarios.UseVisualStyleBackColor = true;
+            // 
+            // tnPrestamos
+            // 
+            this.tnPrestamos.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tnPrestamos.Location = new System.Drawing.Point(304, 311);
+            this.tnPrestamos.Name = "tnPrestamos";
+            this.tnPrestamos.Size = new System.Drawing.Size(200, 40);
+            this.tnPrestamos.TabIndex = 3;
+            this.tnPrestamos.Text = "Gestión de Préstamos";
+            this.tnPrestamos.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tnPrestamos);
+            this.Controls.Add(this.btnUsuarios);
+            this.Controls.Add(this.btnLibros);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sistema de Biblioteca";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLibros;
+        private System.Windows.Forms.Button btnUsuarios;
+        private System.Windows.Forms.Button tnPrestamos;
+    }
+}
+
