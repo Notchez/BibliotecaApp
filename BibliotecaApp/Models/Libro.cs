e94103d80af9560e1e708ce00f08afade73d1013
Tito Nochez <nochez_@hotmail.com>
2026-03-10 11:45:39 -0500

    Reestructuración inicial del proyecto y diseño base de LibrosForm

--- BibliotecaApp/Models/Libro.cs
+++ BibliotecaApp/Models/Libro.cs
@@ -8,10 +8,6 @@ namespace BibliotecaApp.Models
 {
     public class Libro
     {
-        public int Id { get; set; }
-
-        public string Titulo { get; set; }
-
         public string Autor { get; set; }
 
         public int Anio { get; set; }
