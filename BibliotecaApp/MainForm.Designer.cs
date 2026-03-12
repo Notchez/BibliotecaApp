7b80cee524bd0d6b356f3a9b2dd0c9473496c6e2
Tito Nochez <nochez_@hotmail.com>
2026-03-11 21:11:48 -0600

    Eliminacion de label1_Click que fue creada por error

--- BibliotecaApp/MainForm.Designer.cs
+++ BibliotecaApp/MainForm.Designer.cs
@@ -44,7 +44,6 @@
             this.label1.Size = new System.Drawing.Size(449, 57);
             this.label1.TabIndex = 0;
             this.label1.Text = "Sistema de Biblioteca";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // btnLibros
             // 
